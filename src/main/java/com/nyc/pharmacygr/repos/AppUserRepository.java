bbf2d89c620beaee292c1a4c82453de0a78e98bb
SpyrosBlack <34628964+SpyrosBlack@users.noreply.github.com>
2024-12-09 13:13:46 +0200

    Create entities - dtos - repos

---
+++ src/main/java/com/nyc/pharmacygr/repos/AppUserRepository.java
@@ -0,0 +1,7 @@
+package com.nyc.pharmacygr.repos;
+
+import com.nyc.pharmacygr.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, String> {
+}
