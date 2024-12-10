94339963e36b8c87e9e2a06fb615ce7a62afb829
SpyrosBlack <spyrosmavros@gmail.com>
2024-12-10 07:56:46 +0200

    login functionality

--- src/main/java/com/nyc/pharmacygr/repos/AppUserRepository.java
+++ src/main/java/com/nyc/pharmacygr/repos/AppUserRepository.java
@@ -4,4 +4,5 @@ import com.nyc.pharmacygr.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
+    AppUser findByUsernameAndUserpassword(String username, String password);
 }
